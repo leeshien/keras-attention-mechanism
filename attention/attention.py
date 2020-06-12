04ba17056523a9dad2d943aad28bc8aec690b306
Lee Shien <ganleeshien@gmail.com>
2020-06-12 21:47:10 +0800

    Update import function to align with TF version

--- attention/attention.py
+++ attention/attention.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.layers import Dense, Lambda, dot, Activation, concatenate
+import tensorflow.keras.layers
 
 
 def attention_3d_block(hidden_states):
@@ -13,14 +13,14 @@ def attention_3d_block(hidden_states):
     #              hidden_states            dot               W            =>           score_first_part
     # (batch_size, time_steps, hidden_size) dot (hidden_size, hidden_size) => (batch_size, time_steps, hidden_size)
     # W is the trainable weight matrix of attention Luong's multiplicative style score
-    score_first_part = Dense(hidden_size, use_bias=False, name='attention_score_vec')(hidden_states)
+    score_first_part = tensorflow.keras.layers.Dense(hidden_size, use_bias=False, name='attention_score_vec')(hidden_states)
     #            score_first_part           dot        last_hidden_state     => attention_weights
     # (batch_size, time_steps, hidden_size) dot   (batch_size, hidden_size)  => (batch_size, time_steps)
-    h_t = Lambda(lambda x: x[:, -1, :], output_shape=(hidden_size,), name='last_hidden_state')(hidden_states)
-    score = dot([score_first_part, h_t], [2, 1], name='attention_score')
-    attention_weights = Activation('softmax', name='attention_weight')(score)
+    h_t = tensorflow.keras.layers.Lambda(lambda x: x[:, -1, :], output_shape=(hidden_size,), name='last_hidden_state')(hidden_states)
+    score = tensorflow.keras.layers.dot([score_first_part, h_t], [2, 1], name='attention_score')
+    attention_weights = tensorflow.keras.layers.Activation('softmax', name='attention_weight')(score)
     # (batch_size, time_steps, hidden_size) dot (batch_size, time_steps) => (batch_size, hidden_size)
-    context_vector = dot([hidden_states, attention_weights], [1, 1], name='context_vector')
-    pre_activation = concatenate([context_vector, h_t], name='attention_output')
-    attention_vector = Dense(128, use_bias=False, activation='tanh', name='attention_vector')(pre_activation)
+    context_vector = tensorflow.keras.layers.dot([hidden_states, attention_weights], [1, 1], name='context_vector')
+    pre_activation = tensorflow.keras.layers.concatenate([context_vector, h_t], name='attention_output')
+    attention_vector = tensorflow.keras.layers.Dense(128, use_bias=False, activation='tanh', name='attention_vector')(pre_activation)
     return attention_vector
